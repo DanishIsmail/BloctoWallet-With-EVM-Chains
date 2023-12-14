953edbfbf854f909d2dd2230eb17f6f01e453969
DanishIsmail <danish.gakher8@gmail.com>
2023-12-15 01:08:13 +0500

    add the contract interaction

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,13 +1,20 @@
 import { useState } from "react";
-import { connectWallet, disConnectWallet } from "../config";
+import {
+  connectWallet,
+  disConnectWallet,
+  getFeeRate,
+  setFeeRate,
+} from "../config";
 
 const Home = () => {
   let [walletAddress, setWalletAddress] = useState();
+  let [fee, setFee] = useState();
+
   const walletHandler = async () => {
     try {
       let data = await connectWallet();
       setWalletAddress(data);
-      console.log("user account==>", data);
+      // console.log("user account==>", data);
     } catch (error) {
       console.log("error==>", error);
     }
@@ -18,6 +25,19 @@ const Home = () => {
     setWalletAddress();
   };
 
+  const getFeeHandler = async () => {
+    let data = await getFeeRate();
+    setFee(data);
+  };
+
+  const setFeeHandler = async () => {
+    try {
+      let data = await setFeeRate(walletAddress);
+    } catch (error) {
+      console.log("error==>", error);
+    }
+  };
+
   return (
     <>
       {walletAddress ? (
@@ -26,6 +46,12 @@ const Home = () => {
             wallet Address: <span>{walletAddress}</span>
           </h4>
           <button onClick={disconnectWalletHandler}>Disconnect wallet</button>
+          <br></br>
+          <br></br>
+          <button onClick={getFeeHandler}> Get Fee</button>
+          {fee && <p>Fee: {fee}</p>}
+
+          <button onClick={setFeeHandler}> set Fee</button>
         </>
       ) : (
         <button onClick={walletHandler}>connect wallet</button>
--- src/config/index.js
+++ src/config/index.js
@@ -1,16 +1,32 @@
 import BloctoSDK from "@blocto/sdk";
+import Web3 from "web3";
+import abi from "./ABI.json";
+
+let RPC = process.env.REACT_APP_RPC_URL;
 
 const bloctoSDK = new BloctoSDK({
   ethereum: {
     // (required) chainId to be used
     chainId: "0x1",
     // (required) JSON RPC endpoint
-    rpc: process.env.RPC_URL,
+    rpc: RPC,
   },
 
   // (optional) Blocto app ID
   //   appId: "YOUR_BLOCTO_APP_ID",
 });
+
+const web3 = new Web3(bloctoSDK.ethereum);
+const contractAddress = "0x2b2B6b0978c236260614440c75a78Fc9ABB1970e";
+
+const toObject = (object) => {
+  return JSON.parse(
+    JSON.stringify(
+      object,
+      (key, value) => (typeof value === "bigint" ? value.toString() : value) // return everything else unchanged
+    )
+  );
+};
 
 export const connectWallet = async () => {
   try {
@@ -35,4 +51,34 @@ export const disConnectWallet = async () => {
   } catch (error) {
     console.log("error==>", error);
   }
+};
+
+export const setFeeRate = async (account) => {
+  try {
+    const contract = new web3.eth.Contract(abi, contractAddress);
+    const setFee = await contract.methods
+      .setFee("1")
+      .send({ from: account[0] });
+
+    // console.log(`setFee: ${toObject(setFee)}`);
+    return true;
+  } catch (error) {
+    console.log("error==>", error);
+    return null;
+  }
+};
+
+export const getFee = async () => {
+  const contract = new web3.eth.Contract(abi, contractAddress);
+
+  const fee = await contract.methods.getFee().call();
+  console.log(`getFee: ${toObject(fee)}`);
+  return toObject(fee);
+};
+
+export const getFeeRate = async () => {
+  const contract = new web3.eth.Contract(abi, contractAddress);
+  const feeRate = await contract.methods.feeRate().call();
+  console.log(`feeRate: ${toObject(feeRate)}`);
+  return toObject(feeRate);
 };
